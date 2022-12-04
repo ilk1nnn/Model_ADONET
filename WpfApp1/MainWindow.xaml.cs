4265f85b28aa92ec8eba5e231c9b4144d43609b2
Sule_jr92 <Sule_jr92@STHQ012D-02>
2022-12-04 17:35:57 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Entities;
+using System.Data.Entity;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private ObservableCollection<Book> allBooks;
+
+        public ObservableCollection<Book> AllBooks
+        {
+            get { return allBooks; }
+            set { allBooks = value; OnPropertyChanged(); }
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            Get();
+
+        }
+
+        private void Get()
+        {
+            using (var context = new LibraryEntities())
+            {
+
+                var books = context.Books.Include(nameof(Book.Author)).Include(nameof(Book.Category));
+                AllBooks = new ObservableCollection<Book>(books);
+            }
+        }
+
+
+        public void Update()
+        {
+
+            using (var context = new LibraryEntities())
+            {
+
+                var books = context.Books.Include(nameof(Book.Author)).Include(nameof(Book.Category));
+                AllBooks = new ObservableCollection<Book>(books);
+            }
+        }
+
+    }
+}
